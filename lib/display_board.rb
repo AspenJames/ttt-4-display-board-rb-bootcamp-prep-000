eb121455d17b8c7a2d7bfee45ef5e80ca54dd2a5
Aspen James <aspenjames@tqca.org>
2018-02-08 05:00:56 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-# board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 #row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
 #row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
 #row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
@@ -12,3 +12,4 @@ def display_board(board)
   puts line
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+display_board
