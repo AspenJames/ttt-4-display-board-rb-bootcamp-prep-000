82f0dd9d6d0eefe8b5836ea1039a9e23c0d1c9dc
Aspen James <aspenjames@tqca.org>
2018-02-08 05:01:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,9 @@ board = [" "," "," "," "," "," "," "," "," "]
 #row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
 #row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
 #row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-line = "-----------"
+
 def display_board(board)
+  line = "-----------"
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts line
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
